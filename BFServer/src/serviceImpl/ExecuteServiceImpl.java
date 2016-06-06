b7654d3d0b6c6560ca2326d148e92f1abc058cb0
HikariChao <gz120748@gmail.com>
2016-06-06 22:00:56 +0800

    initial commit
    code analysis part

---
+++ BFServer/src/serviceImpl/ExecuteServiceImpl.java
@@ -0,0 +1,132 @@
+//请不要修改本文件名
+package serviceImpl;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import service.ExecuteService;
+import service.UserService;
+
+public class ExecuteServiceImpl implements ExecuteService {
+
+	/**
+	 * 请实现该方法
+	 */
+	@Override
+	public String execute(String code, String param) throws RemoteException {
+		//initialize
+		char[] codeChars = code.toCharArray();
+		String result = "";
+		char[] array = new char[30000]; // use char instead of byte
+		for(int i = 0; i < array.length; i++) {
+			array[i] = 0;
+		}
+		int codePointer = 0;
+		int arrayPointer = 0;
+		int paramPointer = 0;
+		int beginPointer = 0; // for loop
+		int endPointer = 0;
+		
+		//get all the parameters
+		char[] paraChars = param.toCharArray();
+		ArrayList<Character> parameters = new ArrayList<Character>();
+		for(int i = 0; i < paraChars.length; i++) {
+			parameters.add(paraChars[i]);
+		}
+		
+		//scan all begins and ends to make them combined
+		int numOfLoop = 0;
+		for(int i = 0; i < codeChars.length; i++) {
+			if(codeChars[i] == '[') {
+				numOfLoop += 1;
+			}
+		}
+		int [][] loops = new int[numOfLoop][2]; //begin index, end index
+		int loopId = 0;
+		for(int i = 0; i < codeChars.length; i++) {
+			if(codeChars[i] == '[') {
+				loops[loopId][0] = i;
+				int obstacles = 0;
+				for(int j = 1; j < codeChars.length-i; j++) {
+					if(codeChars[i+j] == '[') {
+						obstacles += 1;
+						continue;
+					}
+					if(codeChars[i+j] == ']' && obstacles != 0) {
+						obstacles -= 1;
+						continue;
+					}
+					if(codeChars[i+j] == ']' && obstacles == 0) {
+						loops[loopId][1] = i+j;
+						break;
+					}
+				}
+				loopId += 1;
+			}
+		}
+		
+		//read and execute code one by one
+		while(codePointer < codeChars.length) {
+			switch(codeChars[codePointer]) {
+			case '>':
+				arrayPointer += 1;
+				codePointer += 1;
+				break;
+			case '<':
+				arrayPointer -= 1;
+				codePointer += 1;
+				break;
+			case '+':
+				array[arrayPointer] += 1;
+				codePointer += 1;
+				break;
+			case '-':
+				array[arrayPointer] -= 1;
+				codePointer += 1;
+				break;
+			case '.':
+				result = result + array[arrayPointer];
+				codePointer += 1;
+				break;
+			case ',':
+				array[arrayPointer] = parameters.get(paramPointer);
+				paramPointer += 1;
+				codePointer += 1;
+				break;
+			case '[':
+				int loopIndex = 0;
+				for(int i = 0; i < loops.length; i++) {
+					if(loops[i][0] == codePointer) {
+						loopIndex = i;
+						break;
+					}
+				}
+				endPointer = loops[loopIndex][1];
+				if(array[arrayPointer] == 0) {
+					codePointer = endPointer;
+				} else {
+					codePointer += 1;
+				}
+				break;
+			case ']':
+				int loopIndex2 = 0;
+				for(int i = 0; i < loops.length; i++) {
+					if(loops[i][1] == codePointer) {
+						loopIndex2 = i;
+						break;
+					}
+				}
+				beginPointer = loops[loopIndex2][0];
+				if(array[arrayPointer] != 0) {
+					codePointer = beginPointer;
+				} else {
+					codePointer += 1;
+				}
+				break;				
+			}
+		}
+		return result;
+	}
+
+}
---
+++ BFServer/src/serviceImpl/Test.java
@@ -0,0 +1,14 @@
+package serviceImpl;
+//waiting to be deleted
+
+import java.rmi.RemoteException;
+
+public class Test {
+	public static void main(String arg[]) throws RemoteException {
+		ExecuteServiceImpl executeService = new ExecuteServiceImpl();
+		String result = executeService.execute(",>++++++[<-------->-],,[<+>-],<.>.", "4 3\n");
+		System.out.println(result);
+		String result1 = executeService.execute("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.", "");
+		System.out.println(result1);
+	}
+}
